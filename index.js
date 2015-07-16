93076aa4ee28d0135d7f4f3a8629df56cb530c32
tfennelly <tom.fennelly@gmail.com>
2015-07-16 15:30:05 +0100

    0.0.2-alpha-7 - multi bundles

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ var source = require('vinyl-source-stream');
 var transformTools = require('browserify-transform-tools');
 var _string = require('underscore.string');
 
-
-var bundleModule;
-var bundleOutputFile;
-var bundleToAdjunctPackageDir;
-var bundleAsJenkinsModule = false;
-var bundleTransforms;
+var bundles = []; // see exports.bundle function
 
 var adjunctBasePath = './target/generated-adjuncts/';
 var jsmodulesBasePath = './src/main/webapp/jsmodules/';
 
 var srcPath = './js';
 var testSrcPath = './spec';
-var lessSrcPath = undefined;
 
 exports.gulp = gulp;
 
@@ -61,10 +55,6 @@ exports.tests = function(path) {
     testSrcPath = normalizePath(path);
 };
 
-exports.less = function(path) {
-    lessSrcPath = path;
-};
-
 function normalizePath(path) {
     path = _string.ltrim(path, './')
     path = _string.ltrim(path, '/')
@@ -76,112 +66,70 @@ function packageToPath(packageName) {
     return _string.replaceAll(packageName, '.', '/');
 }
 
-var moduleMappings = [];
-var requireTransform = transformTools.makeRequireTransform("requireTransform",
-    {evaluateArguments: true},
-    function(args, opts, cb) {
-        var required = args[0];
-
-        for (var i = 0; i < moduleMappings.length; i++) {
-            var mapping = moduleMappings[i];
-            if (mapping.from === required) {
-                if (mapping.require) {
-                    return cb(null, "require('" + mapping.require + "')");
-                } else {
-                    return cb(null, "require('jenkins-modules').require('" + mapping.to + "')");
-                }
-            }
-        }
-        return cb();
-    });
-
-var isFirstModule = true;
-var importWrapperTransform = transformTools.makeStringTransform("importWrapperTransform", {},
-    function (content, opts, done) {
-        if (isFirstModule) {
-            var mappings = "";
-            for (var i = 0; i < moduleMappings.length; i++) {
-                var mapping = moduleMappings[i];
-                if (mappings.length > 0) {
-                    mappings += ", ";
-                }
-                mappings += "'" + mapping.to + "'";
-            }
-            var wrappedContent = 
-                "require('jenkins-modules')\n" +
-                "    .import(" + mappings + ")\n" +
-                "    .then(function() {\n" +
-                "\n" +
-                content +
-                "\n" +
-                "    });\n";
-
-            isFirstModule = false;
-            return done(null, wrappedContent);
-        } else {
-            return done(null, content);
-        }
-    });
-
 exports.bundle = function(modulePath, as) {
     if (!modulePath) {
         gutil.log(gutil.colors.red("Error: Invalid bundle registration for module 'modulePath' must be specify."));
         throw "'bundle' registration failed. See error above.";
     }
+
+    var bundle = {};
+
+    bundle.js = _string.strRightBack(modulePath, '/'); // The short name of the javascript file (with extension but without path) 
+    bundle.module = _string.rtrim(bundle.js, '.js'); // The short name with the .js extension removed
+    
     if (!as) {
-        var lastSlash = modulePath.indexOf('/');
-        
-        if (lastSlash === -1) {
-            as = modulePath;
-        } else {
-            as = _string.strRightBack(modulePath, '/');
-        } 
+        as = bundle.js;
     }
-    
-    bundleModule = modulePath;
-    bundleOutputFile = as;
-    
-    var options = {
-        inAdjunctPackage: function(packageName) {
-            if (!packageName) {
-                gutil.log(gutil.colors.red("Error: Invalid bundle registration for module '" + modulePath + "'. You can't specify a 'null' adjunct package name."));
-                throw "'bundle' registration failed. See error above.";
-            }
-            bundleToAdjunctPackageDir = packageToPath(packageName);
-            gutil.log("Bundle will be generated as an adjunct in '" + adjunctBasePath + "' as '" + packageName + "." + _string.rtrim(as, '.js') + "' (.js file).");
-            return options;
-        },
-        asJenkinsModuleResource: function() {
-            bundleAsJenkinsModule = true;
-            gutil.log("Bundle will be generated as a Jenkins Module in '" + jsmodulesBasePath + "' as '" + as + "'.");            
-            return options;
-        },
-        withTransforms: function(transforms) {
-            bundleTransforms = transforms;
-            return options;
-        },
-        withExternalModuleMapping: function(from, to, require) {
-            if (!from || !to) {
-                var message = "Cannot call 'withExternalModuleMapping' without defining both 'to' and 'from' module names.";
-                exports.logError(message);
-                throw message;
-            }
-            
-            // special case because we are externalizing handlebars runtime for handlebarsify.
-            if (from === 'handlebars' && to === 'handlebars:handlebars3' && !require) {
-                require = 'jenkins-handlebars-rt/runtimes/handlebars3_rt';
-            }
-            
-            moduleMappings.push({
-                from: from, 
-                to: to, 
-                require: require
-            });
-            return options;
-        }        
+
+    bundle.bundleModule = modulePath;
+    bundle.bundleOutputFile = as;
+    bundle.moduleMappings = [];
+    bundle.inAdjunctPackage = function(packageName) {
+        if (!packageName) {
+            gutil.log(gutil.colors.red("Error: Invalid bundle registration for module '" + modulePath + "'. You can't specify a 'null' adjunct package name."));
+            throw "'bundle' registration failed. See error above.";
+        }
+        bundle.bundleToAdjunctPackageDir = packageToPath(packageName);
+        gutil.log("Bundle will be generated as an adjunct in '" + adjunctBasePath + "' as '" + packageName + "." + bundle.module + "' (it's a .js file).");
+        return bundle;
+    };
+    bundle.asJenkinsModuleResource = function() {
+        bundle.bundleAsJenkinsModule = true;
+        gutil.log("Bundle will be generated as a Jenkins Module in '" + jsmodulesBasePath + "' as '" + as + "'.");            
+        return bundle;
+    };
+    bundle.withTransforms = function(transforms) {
+        bundle.bundleTransforms = transforms;
+        return bundle;
+    };
+    bundle.withExternalModuleMapping = function(from, to, require) {
+        if (!from || !to) {
+            var message = "Cannot call 'withExternalModuleMapping' without defining both 'to' and 'from' module names.";
+            exports.logError(message);
+            throw message;
+        }
+        
+        // special case because we are externalizing handlebars runtime for handlebarsify.
+        if (from === 'handlebars' && to === 'handlebars:handlebars3' && !require) {
+            require = 'jenkins-handlebars-rt/runtimes/handlebars3_rt';
+        }
+        
+        bundle.moduleMappings.push({
+            from: from, 
+            to: to, 
+            require: require
+        });
+        
+        return bundle;
+    };            
+    bundle.less = function(path) {
+        bundle.lessSrcPath = path;
+        return bundle;
     };
     
-    return options;
+    bundles.push(bundle);
+    
+    return bundle;
 };
 
 exports.logInfo = function(message) {
@@ -218,55 +166,116 @@ var tasks = {
             .pipe(jasmine({reporter: [terminalReporter, junitReporter]}));
     },
     bundle: function() {
-        if (!bundleModule) {
-            gutil.log(gutil.colors.red("Error: Cannot perform 'bundle' task. No bundle 'module' is registered. You must call require('jenkins-js-build').bundle([module]) in gulpfile.js, specifying the bundle 'module'."));
+        if (bundles.length === 0) {
+            gutil.log(gutil.colors.red("Error: Cannot perform 'bundle' task. No 'module' bundles are registered. You must call require('jenkins-js-build').bundle([module]) in gulpfile.js, specifying at least one bundle 'module'."));
             throw "'bundle' task failed. See error above.";
-        }
-        if (!bundleToAdjunctPackageDir && !bundleAsJenkinsModule) {
-            gutil.log(gutil.colors.red("Error: Cannot perform 'bundle' task. No bundle output sec defined. You must call 'inAdjunctPackage([adjunct-package-name])' or 'asJenkinsModuleResource' on the response return from the call to 'bundle'."));
-            throw "'bundle' task failed. See error above.";
-        }
-
-        var bundleTo;
-        if (bundleAsJenkinsModule) {
-            bundleTo = jsmodulesBasePath;
-        } else {
-            bundleTo = adjunctBasePath + "/" + bundleToAdjunctPackageDir;
-        }
-
-        if (lessSrcPath) {
-            var less = require('gulp-less');
-            gulp.src(lessSrcPath)
-                .pipe(less())
-                .pipe(gulp.dest(bundleTo));
         }
         
-        var bundler = browserify({
-            entries: [bundleModule],
-            extensions: ['.js', '.hbs'],
-            cache: {},
-            packageCache: {},
-            fullPaths: false
-        });
-        var hbsfy = require("hbsfy").configure({
-            compiler: "require('jenkins-handlebars-rt/runtimes/handlebars3_rt')"
-        });
-        bundler.transform(hbsfy);        
-        if (bundleTransforms) {
-            for (var i = 0; i < bundleTransforms.length; i++) {
-                bundler.transform(bundleTransforms[i]);        
+        // Bundle all bundles.
+        for (var i = 0; i < bundles.length; i++) {
+            var bundle = bundles[i];
+            
+            if (!bundle.bundleToAdjunctPackageDir && !bundle.bundleAsJenkinsModule) {
+                gutil.log(gutil.colors.red("Error: Cannot perform 'bundle' task. No bundle output sec defined. You must call 'inAdjunctPackage([adjunct-package-name])' or 'asJenkinsModuleResource' on the response return from the call to 'bundle'."));
+                throw "'bundle' task failed. See error above.";
             }
+    
+            var bundleTo;
+            if (bundle.bundleAsJenkinsModule) {
+                bundleTo = jsmodulesBasePath;
+            } else {
+                bundleTo = adjunctBasePath + "/" + bundle.bundleToAdjunctPackageDir;
+            }
+    
+            if (bundle.lessSrcPath) {
+                var less = require('gulp-less');
+                var lessBundleTo = bundleTo;
+                
+                if (bundle.bundleAsJenkinsModule) {
+                    // If it's a jenkins module, the CSS etc need to go into a folder under jsmodulesBasePath
+                    // and the name of the folder must be the module name
+                    lessBundleTo += '/' + bundle.module;
+                }
+                
+                gulp.src(bundle.lessSrcPath)
+                    .pipe(less())
+                    .pipe(gulp.dest(lessBundleTo));
+            }
+            
+            var bundler = browserify({
+                entries: [bundle.bundleModule],
+                extensions: ['.js', '.hbs'],
+                cache: {},
+                packageCache: {},
+                fullPaths: false
+            });
+            var hbsfy = require("hbsfy").configure({
+                compiler: "require('jenkins-handlebars-rt/runtimes/handlebars3_rt')"
+            });
+            bundler.transform(hbsfy);        
+            if (bundle.bundleTransforms) {
+                for (var i = 0; i < bundle.bundleTransforms.length; i++) {
+                    bundler.transform(bundle.bundleTransforms[i]);        
+                }
+            }
+            if (bundle.moduleMappings.length > 0) {
+                addModuleMappingTransforms(bundle.moduleMappings, bundler);
+            }
+            
+            bundler.bundle().pipe(source(bundle.bundleOutputFile))
+                .pipe(gulp.dest(bundleTo));            
         }
-        if (moduleMappings.length > 0) {
-            bundler.transform(requireTransform);
-            bundler.transform(importWrapperTransform);
-        }
-        
-        return bundler.bundle().pipe(source(bundleOutputFile))
-            .pipe(gulp.dest(bundleTo));
-
     },
     rebundle: function() {
         gulp.watch(['./index.js', srcPath + '/**/*.js', srcPath + '/**/*.hbs'], ['bundle']);
     }
 };
+
+function addModuleMappingTransforms(moduleMappings, bundler) {
+    var requireTransform = transformTools.makeRequireTransform("requireTransform",
+        {evaluateArguments: true},
+        function(args, opts, cb) {
+            var required = args[0];
+            for (var i = 0; i < moduleMappings.length; i++) {
+                var mapping = moduleMappings[i];
+                if (mapping.from === required) {
+                    if (mapping.require) {
+                        return cb(null, "require('" + mapping.require + "')");
+                    } else {
+                        return cb(null, "require('jenkins-modules').require('" + mapping.to + "')");
+                    }
+                }
+            }
+            return cb();
+        });
+    var importWrapApplied = false;
+    var importWrapperTransform = transformTools.makeStringTransform("importWrapperTransform", {},
+        function (content, opts, done) {
+            if (!importWrapApplied) {
+                var mappings = "";
+                for (var i = 0; i < moduleMappings.length; i++) {
+                    var mapping = moduleMappings[i];
+                    if (mappings.length > 0) {
+                        mappings += ", ";
+                    }
+                    mappings += "'" + mapping.to + "'";
+                }
+                var wrappedContent = 
+                    "require('jenkins-modules')\n" +
+                    "    .import(" + mappings + ")\n" +
+                    "    .then(function() {\n" +
+                    "\n" +
+                    content +
+                    "\n" +
+                    "    });\n";
+    
+                importWrapApplied = true;
+                return done(null, wrappedContent);
+            } else {
+                return done(null, content);
+            }
+        });    
+
+    bundler.transform(requireTransform);
+    bundler.transform(importWrapperTransform);
+}
