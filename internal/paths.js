dd13d0b51c7836177b10fbd67e566e51a102ca76
tfennelly <tom.fennelly@gmail.com>
2016-02-24 15:39:15 +0000

    Extract path info in paths.js

--- internal/paths.js
+++ internal/paths.js
@@ -1,9 +1,6 @@
 var logger = require('./logger');
 var maven = require('./maven');
 
-exports.srcPaths;
-exports.testSrcPath;
-
 if (maven.isMavenProject) {
     exports.srcPaths = ['src/main/js','src/main/less'];
     exports.testSrcPath = 'src/test/js';    
