228113f90f725b4e86498131f73b5615aaa9b15f
tfennelly <tom.fennelly@gmail.com>
2017-09-05 12:41:13 +0100

    Generate full dependency bundles

--- spec/adjunctexternal-spec.js
+++ spec/adjunctexternal-spec.js
@@ -15,14 +15,14 @@ describe("adjunctexternal test", function () {
         expect(fs.existsSync(generatedBundleFile)).toBe(true);
         
         var fileContents = fs.readFileSync(generatedBundleFile, 'utf8');
-        console.log(fileContents);
+        // console.log(fileContents);
         
         // Check that the file contains an export for the module and that the names used in the export are
         // properly normalized i.e. no "@jenkins-cd" etc
-        var indexOfPart = fileContents.indexOf("___$$$___doExport('jenkins-cd-js-modules', 'jenkins-cd-js-modules@0.0.8', require('@jenkins-cd/js-modules'));");
+        var indexOfPart = fileContents.indexOf("___$$$___doExport('jenkins-cd-js-modules', 'jenkins-cd-js-modules@0.0.8', entryModule);");
         expect(indexOfPart !== -1).toBe(true);
         // And that the 'any' version is exported.
-        indexOfPart = fileContents.indexOf("___$$$___doExport('jenkins-cd-js-modules', 'jenkins-cd-js-modules@any', require('@jenkins-cd/js-modules'));");
+        indexOfPart = fileContents.indexOf("___$$$___doExport('jenkins-cd-js-modules', 'jenkins-cd-js-modules@any', entryModule);");
         expect(indexOfPart !== -1).toBe(true);
     });
 });
